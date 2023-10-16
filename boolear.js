b8fa1db5a49850af535ac2b07906e0f59280eb1a
John Doe <johndoe@example.com>
2023-10-16 15:10:19 +0300

    add: create boolean.js and template strings

---
+++ boolear.js
@@ -0,0 +1,5 @@
+const firstNumber = 85
+const secondNumber = 38
+if (firstNumber > secondNumber)
+    {console.log (`верно, число ${firstNumber} больше числа ${secondNumber}`)}
+else {console.log (`неверно, число ${firstNumber} меньше числа ${secondNumber}`)}
--- var.js
+++ var.js
@@ -1,1 +1,9 @@
 const name = 'Petia'
+const firstNumber = 7
+const secondNumber = 8
+const firstWord = 'привет'
+const secondWord = 'мир'
+const resultNumbersSum = firstNumber + secondNumber
+const resultConcutWord = firstWord + ' ' + secondWord
+console.log(resultNumbersSum)
+console.log(resultConcutWord)
